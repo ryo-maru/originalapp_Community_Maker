f2a805560b8c3f9ccb03794c7e49b375293deff1
maruyamaryo <t.t.uncle0803@gmail.com>
2021-08-17 13:27:59 +0900

    communities_controller修正

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,6 +1,6 @@
 class CommunitiesController < ApplicationController
   before_action :set_community, only: %i[ show edit update destroy ]
-  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
+  #before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
 
   # GET /communities or /communities.json
   def index
