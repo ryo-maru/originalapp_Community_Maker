b9a71962df32c8e28cf3479c9e35ee5d95c465bd
maruyamaryo <t.t.uncle0803@gmail.com>
2021-08-17 14:26:20 +0900

    Topページ作成

--- app/mailers/community_mailer.rb
+++ app/mailers/community_mailer.rb
@@ -1,6 +1,6 @@
 class CommunityMailer < ApplicationMailer
   def community_mail(community)
    @community = community
-   mail to: "t.t.uncle0803@gmail.com", subject: "お問い合わせの確認メール"
+   mail to: "t.t.uncle0803@gmail.com", subject: "新規コミュニティ作成確認メール"
  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :communities
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  root 'communities#index'
+  root 'tops#index'
 end
