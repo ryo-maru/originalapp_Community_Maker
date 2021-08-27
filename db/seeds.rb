221e9c553b5a1f91d9686a4a911a40bfab768fd5
maruyamaryo <t.t.uncle0803@gmail.com>
2021-08-27 23:44:45 +0900

    画像追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -76,13 +76,13 @@ Community.create!(
  name: "加藤純一最強！加藤純一最強！",
  description: "全国の衛門集合だ！！",
  user_id: 6,
- image: open("./db/fixtures/ダウンロード.jpeg")
+ image: open("./db/fixtures/download.jpeg")
 )
 Community.create!(
  name: "Among usで遊びましょう",
  description: "初心者もベテランも大歓迎",
  user_id: 7,
- image: open("./db/fixtures/ダウンロード.png")
+ image: open("./db/fixtures/download.png")
 )
 
 Community.create!(
@@ -96,21 +96,21 @@ Community.create!(
  name: "細美武士好き♪",
  description: "音楽フェスではかぶりつき♪",
  user_id: 9,
- image: open("./db/fixtures/ダウンロードtakesi.jpeg")
+ image: open("./db/fixtures/downloadtakesi.jpeg")
 )
 
 Community.create!(
  name: "檜山沙耶ファンクラブ",
  description: "ウェザーニュースキャスターのさやっち応援コミュニティです",
  user_id: 10,
- image: open("./db/fixtures/ダウンロードsaya.jpeg")
+ image: open("./db/fixtures/downloadsaya.jpeg")
 )
 
 Community.create!(
  name: "集まれ！暴力の森",
  description: "凶暴な人集まれ！！",
  user_id: 11,
- image: open("./db/fixtures/ダウンロード23.jpeg")
+ image: open("./db/fixtures/download23.jpeg")
 )
 
 Community.create!(
@@ -131,7 +131,7 @@ Community.create!(
 name: "横浜住みで釣り好きな人",
 description: "釣った魚の報告や、釣り場情報の共有しましょう",
 user_id: 14,
-image: open("./db/fixtures/ダウンロード44.jpeg")
+image: open("./db/fixtures/download44.jpeg")
 )
 
 Community.create!(
@@ -145,28 +145,28 @@ Community.create!(
 name: "2021年のフジロックに参加した人",
 description: "思い出共有しましょう、来年に向けての反省点など",
 user_id: 16,
-image: open("./db/fixtures/ダウンロード88.png")
+image: open("./db/fixtures/download88.png")
 )
 
 Community.create!(
 name: "本好きな人",
 description: "最近読んで良かった本の共有や、好きな本について語りたいです",
 user_id: 17,
-image: open("./db/fixtures/ダウンロード99.jpeg")
+image: open("./db/fixtures/download99.jpeg")
 )
 
 Community.create!(
 name: "スカイロケットカンパニー社員",
 description: "スカロケ好きな人、みんな愛してるよ",
 user_id: 18,
-image: open("./db/fixtures/ダウンロード56.jpeg")
+image: open("./db/fixtures/download56.jpeg")
 )
 
 Community.create!(
 name: "結婚生活の悩み",
 description: "友人に相談できないこと、何でも聞きます",
 user_id: 19,
-image: open("./db/fixtures/ダウンロード000.jpeg")
+image: open("./db/fixtures/download000.jpeg")
 )
 
 Community.create!(
@@ -180,7 +180,7 @@ Community.create!(
 name: "キャンプにいきたい",
 description: "おすすめキャンプ場や、思い出の共有に使ってください",
 user_id: 21,
-image: open("./db/fixtures/ダウンロード1223.jpeg")
+image: open("./db/fixtures/download1223.jpeg")
 )
 
 
